27ef9f3936de466a25edcd2bf4313d0479cfa6ed
apocalypseDreamer <damntheguitars@gmail.com>
2022-10-13 20:51:49 +0200

    create hero banner and nav

---
+++ index.js
@@ -0,0 +1,10 @@
+const handleCollapsibleMenu = () => {
+	const collapsibleControl = document.querySelector('#collapse-control');
+	const navUlElement = document.querySelector('nav ul');
+
+	collapsibleControl.addEventListener('click', () => {
+		navUlElement.classList.toggle('expanded');
+	});
+};
+
+handleCollapsibleMenu();
